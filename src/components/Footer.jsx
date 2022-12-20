d045fe01d039f296b0484d8362cdc8da68c26835
pavan sai <pavansssai2001@gmail.com>
2022-12-20 23:49:59 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,21 +7,28 @@ import newLogo from "../assets/newlogo.jpg"
 
 const Footer = () => {
     return (
-        <div className="w-screen h-[45vh] bg-bhc_green flex items-center justify-center flex-col space-y-5 relative">
-            <div className="flex space-x-4 items-center">
-                <img src="https://bhc-web-ker3.vercel.app/assets/img/logo.jpeg" alt="" className="w-16 h-16 shadow-2xl rounded-xl" />
-                <span className="text-4xl font-semibold text-white font-playfair">Bride Health Care</span>
-            </div>
-            <span className="text-bhc_white tracking-wide w-[40%] text-center ">Providing remotely accessible diagnostic parameters for timely healthcare at every corner of the world.</span>
-            <div className="flex space-x-5 relative">
-                <span className="bg-bhc_white text-bhc_green rounded-lg p-1 cursor-pointer duration-300 hover:bg-bhc_green hover:text-white hover:border-white border-2 border-bhc_green">
-                    <TwitterIcon />
-                </span>
-                <span className="bg-bhc_white text-bhc_green rounded-lg p-1 cursor-pointer duration-300 hover:bg-bhc_green hover:text-white hover:border-white border-2 border-bhc_green">
-                    <LinkedInIcon />
-                </span>
-                <span className="bg-bhc_white text-bhc_green rounded-lg p-1 cursor-pointer duration-300 hover:bg-bhc_green hover:text-white hover:border-white border-2 border-bhc_green">
-                    <InstagramIcon />
+        <div className="w-screen h-[45vh] flex">
+            <div className="w-1/3 h-full bg-bhc_green flex flex-col space-y-10 relative py-10 px-2 border-r-2 border-emerald-900">
+                <div className="flex space-x-4 items-center justify-center">
+                    <img src={newLogo} alt="" className="w-16 h-16 shadow-2xl rounded-xl" />
+                    <span className="flex flex-col space-y-1">
+                        <span className="text-4xl font-semibold text-white font-playfair">Bride Health Care</span>
+                        <span className='text-bhc_white font-lora text-lg text-center'>NO DELAY IN HEALTH CARE</span>
+                    </span>
+                </div>
+                <div className="flex space-x-5 relative justify-center">
+                    <span className="bg-bhc_white text-bhc_green rounded-lg p-1 cursor-pointer duration-300 hover:bg-bhc_green hover:text-white hover:border-white border-2 border-bhc_green">
+                        <TwitterIcon />
+                    </span>
+                    <span className="bg-bhc_white text-bhc_green rounded-lg p-1 cursor-pointer duration-300 hover:bg-bhc_green hover:text-white hover:border-white border-2 border-bhc_green">
+                        <LinkedInIcon />
+                    </span>
+                    <span className="bg-bhc_white text-bhc_green rounded-lg p-1 cursor-pointer duration-300 hover:bg-bhc_green hover:text-white hover:border-white border-2 border-bhc_green">
+                        <InstagramIcon />
+                    </span>
+                </div>
+                <span className="text-center text-bhc_white  text-xs tracking-widest absolute bottom-3 left-10">
+                    © Copyright Bridge Healthcare. All Rights ReservedDesigned by I&T
                 </span>
             </div>
             <div className="bg-bhc_green flex flex-col flex-grow justify-center items-center space-y-10">
